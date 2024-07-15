9021fa5995d03ba6c6c65a5575a1cb933f6efbf1
Abhineshwar Jaiswar <131228879+OttoRadiant@users.noreply.github.com>
2024-07-15 10:07:44 +0530

    Add files via upload
    we usually use enums for processes.

---
+++ enumerator/demo.java
@@ -0,0 +1,29 @@
+enum Status {
+    Running, Failed, Pending, Success;
+}
+
+
+
+public class demo {
+    public static void main(String[] args) {
+    
+        // int i = 5;
+        // Status s = Status.Running;
+        // System.out.println(s + " : " +   s.ordinal()); // This will print status and order no.
+
+
+
+        // int j = 10;
+        // Status [] ss = Status.values();
+        // System.out.println(ss[0]);      // This will return acordint to values.
+
+
+        int k = 5;
+        Status ss[] = Status.values();
+        for (Status s:ss) 
+        {
+            System.out.println(s + ": " + s. ordinal());
+        }
+    }
+
+}
