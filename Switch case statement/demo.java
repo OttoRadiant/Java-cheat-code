ada88ecf1b132c6a1bdd50456b7f64ab4f05b950
Abhineshwar Jaiswar <131228879+OttoRadiant@users.noreply.github.com>
2024-07-17 16:06:58 +0530

    Update demo.java

--- Switch case statement/demo.java
+++ Switch case statement/demo.java
@@ -1,7 +1,7 @@
 
 
 
-
+//  This is the simplest Switch Case statement.
 
 public class demo {
     public static void main(String[] args) {
@@ -10,7 +10,7 @@ public class demo {
     String result = "";
 
     switch (day) {
-        case "Tuesday" -> result = "10am "; 
+        case "Tuesday" -> result = "10am ";    //You can use operators for such operations
         
         case "Wednesday" -> result = "11am";
             
